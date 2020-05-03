8d1a11cd92405a140a38b1b7f74dadb6658a3976
yashterror7 <yashbhattm37@gmail.com>
2020-05-03 20:45:14 +0530

    classic angry birds game completed

--- Slingshot.js
+++ Slingshot.js
@@ -5,7 +5,11 @@ class SlingShot{
             pointB: pointB,
             stiffness: 0.04,
             length: 10
+
         }
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.sling3=loadImage("sprites/sling3.png");
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
@@ -14,13 +18,30 @@ class SlingShot{
     fly(){
         this.sling.bodyA = null;
     }
-
+    
     display(){
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
+
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
+            push();
+            
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<220){
+                strokeWeight(7);
+                stroke(91,6,6);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+                image (this.sling3,pointA.x-30,pointA.y-10,15,30);
+            }else {strokeWeight(3);
+                stroke(91,6,6);
+                line(pointA.x+20, pointA.y, pointB.x+20, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x-20, pointB.y);
+                image (this.sling3,pointA.x+20,pointA.y-10,15,30);
+            }
+            
+            pop();
         }
     }
     
